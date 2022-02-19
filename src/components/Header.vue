98745dad599363bdb57bbaa44dc872164eb56b5d
amorellb <amorellb@cifpfbmoll.eu>
2022-02-19 12:26:52 +0100

    feat: create Header component and add it to Home and About pages

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container" id="nav">
+    <img alt="Muscle maker logo" src="../assets/logo-muscle-100.png" />
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
+  <Header />
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "About",
+  components: {
+    Header,
+  },
+};
+</script>
