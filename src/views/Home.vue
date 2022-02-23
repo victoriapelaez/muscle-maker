16239aef9e79ac534f1afea9947f1f6a6d986814
Victoria <mpelaez@cifpfbmoll.eu>
2022-02-23 12:48:40 +0100

    do coachs-section as component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -123,33 +123,7 @@
     </div>
 
     <div class="third-section py-4">
-      <div class="container row m-auto">
-        <h2 class="col-12 my-5 pb-3 text-center text-success border-bottom border-success">EQUIPO</h2>
-        <div class="card col-xl-6 col-md-12 border-0">
-          <img
-            src="../assets/entrenadora-250.jpg"
-            class="card-img-top p-1 rounded-circle m-auto"
-            alt="card-img-top"
-            style="width: 250px; height: 250px"
-          />
-          <div class="card-body m-auto">
-            <h5 class="card-title text-center border-bottom border-warning">Maria Bernat</h5>
-            <p class="card-text text-center">Nutricionista</p>
-          </div>
-        </div>
-        <div class="card col-xl-6 col-md-12 border-0">
-          <img
-            src="../assets/entrenador-250.jpg"
-            class="card-img-top p-1 rounded-circle m-auto"
-            alt="card-img-top"
-            style="width: 250px; height: 250px"
-          />
-          <div class="card-body m-auto">
-            <h5 class="card-title text-center border-bottom border-warning">Jaume Bestard</h5>
-            <p class="card-text text-center">Entrenador</p>
-          </div>
-        </div>
-      </div>
+      <Coachs />
     </div>
     <div class="fourth-section py-5">
       <div class="container m-auto">
@@ -185,8 +159,9 @@
 
 <script>
 import Contact from "../components/Contact.vue";
+import Coachs from "../components/Coachs.vue";
 export default {
   name: "Home",
-  components: { Contact },
+  components: { Contact, Coachs },
 };
 </script>
