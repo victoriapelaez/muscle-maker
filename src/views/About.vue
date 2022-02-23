e7ff9937fb39dba4713d31d60f1ed7b6c3ec8332
Victoria <mpelaez@cifpfbmoll.eu>
2022-02-23 13:00:44 +0100

    initial curriculum coachs

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,54 @@
     <h1>CONÓCENOS</h1>
   </div>
   <Coachs />
+  <div class="row m-auto pt-5 w-75">
+    <div class="col-6 text-center">
+      <h4 class="text-success py-3">EXPERIENCIA PROFESIONAL</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+      <h4 class="text-success py-3">FORMACIÓN ACÁDEMICA</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+      <h4 class="text-success py-3">ÉXITOS PROFESIONALES</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+    </div>
+    <div class="col-6 text-center">
+      <h4 class="text-success py-3">EXPERIENCIA PROFESIONAL</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+      <h4 class="text-success py-3">FORMACIÓN ACÁDEMICA</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+      <h4 class="text-success py-3">ÉXITOS PROFESIONALES</h4>
+      <ul class="list-unstyled">
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+        <li>Entrenador personal Gimnasio Élite 05/2019-10/2021</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 import Coachs from "../components/Coachs.vue";
 export default {
-    name: "About",
-    components: { Coachs }
+  name: "About",
+  components: { Coachs }
 };
 </script>
