e82ed20b7da3b8958f50fd287f871c8b59ee8178
Victoria <mpelaez@cifpfbmoll.eu>
2022-02-25 22:14:03 +0100

    add margin top to title

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container m-auto">
-    <h2 class="col-12 mb-5 pb-3 text-center text-success border-bottom border-success">EQUIPO</h2>
+    <h2 class="col-12 my-5 py-3 text-center text-success border-bottom border-success">EQUIPO</h2>
     <div class="row">
       <div class="card col-12 col-md-6 border-0">
         <img
